1d4223df90aa03f8b5bf239fae1baad29f69cd54
nextlabSangYun <sangyun.kim@nextlab.co.kr>
2018-09-20 16:42:17 +0900

    속성 테스트 완료

--- testDjango/urls.py
+++ testDjango/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.contrib.auth import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path('blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/', include('django.contrib.auth.urls'), name='login'),
+    url(r'^accounts/logout/', include('django.contrib.auth.urls'), name='logout', kwargs={'next_page' : '/'}),
 ]
